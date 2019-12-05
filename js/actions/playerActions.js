1b7605c2a425dfa53fec28bb03adc2b480eec4f1
Katie Wright <katie.wrght@gmail.com>
2019-12-05 12:06:44 +0100

    remove unused sprites

--- js/actions/playerActions.js
+++ js/actions/playerActions.js
@@ -12,16 +12,6 @@ function playerActions(anims) {
   anims.create({
     key: 'player-x-walk-grass',
     frames: anims.generateFrameNumbers('player', {
-      start: 16,
-      end: 19,
-      zeroPad: 19,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
-  anims.create({
-    key: 'player-up-walk',
-    frames: anims.generateFrameNumbers('player', {
       start: 12,
       end: 15,
       zeroPad: 1,
@@ -30,17 +20,7 @@ function playerActions(anims) {
     repeat: -1,
   });
   anims.create({
-    key: 'player-up-walk-grass',
-    frames: anims.generateFrameNumbers('player', {
-      start: 28,
-      end: 31,
-      zeroPad: 31,
-    }),
-    frameRate: 10,
-    repeat: -1,
-  });
-  anims.create({
-    key: 'player-down-walk',
+    key: 'player-up-walk',
     frames: anims.generateFrameNumbers('player', {
       start: 8,
       end: 11,
@@ -50,11 +30,31 @@ function playerActions(anims) {
     repeat: -1,
   });
   anims.create({
+    key: 'player-up-walk-grass',
+    frames: anims.generateFrameNumbers('player', {
+      start: 20,
+      end: 23,
+      zeroPad: 1,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  anims.create({
+    key: 'player-down-walk',
+    frames: anims.generateFrameNumbers('player', {
+      start: 4,
+      end: 7,
+      zeroPad: 1,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  anims.create({
     key: 'player-down-walk-grass',
     frames: anims.generateFrameNumbers('player', {
-      start: 24,
-      end: 27,
-      zeroPad: 27,
+      start: 16,
+      end: 19,
+      zeroPad: 1,
     }),
     frameRate: 10,
     repeat: -1,

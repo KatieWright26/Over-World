d64648d188c1a32fb8b0a1748bc12f6f687c4721
Katie Wright <katie.wrght@gmail.com>
2019-12-05 15:13:11 +0100

    add home scene to map

--- js/scenes/homeScene.js
+++ js/scenes/homeScene.js
@@ -1,4 +1,10 @@
 import Phaser from 'phaser';
+import Player from '../player';
+import { detectDoor } from '../actions/doorDetection';
+
+function detectGrass(_, tile) {
+  this.player.isOnGrass(tile.properties.grass);
+}
 
 export default class HomeScene extends Phaser.Scene {
   constructor() {
@@ -6,11 +12,54 @@ export default class HomeScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('home-tileset', './assets/tilesets/home-tileset.png');
+    this.load.image(
+      'home-tileset',
+      './assets/tilesets/home-tileset.png',
+      16,
+      16
+    );
     this.load.tilemapTiledJSON('home-map', './assets/tilemaps/home.json');
   }
 
-  create() {}
+  create() {
+    const scene = this;
+    const homeMap = this.make.tilemap({ key: 'home-map' });
+    const tileset = homeMap.addTilesetImage('home-tileset', 'home-tileset');
+    const belowPlayer = homeMap.createStaticLayer('below player', tileset);
 
-  upload() {}
+    this.homeWorld = homeMap.createStaticLayer('world', tileset);
+
+    const spawnPoint = homeMap.findObject(
+      'Objects',
+      obj => obj.name === 'Spawn Point'
+    );
+
+    this.player = new Player(scene, spawnPoint.x, spawnPoint.y);
+    belowPlayer.setCollisionByProperty({ collides: true });
+    this.physics.world.addCollider(this.player.sprite, belowPlayer);
+    this.homeWorld.setCollisionByProperty({ collides: true });
+    this.physics.world.addCollider(this.player.sprite, this.homeWorld);
+
+    this.cameras.main.startFollow(this.player.sprite);
+    this.cameras.main.setBounds(
+      0,
+      0,
+      homeMap.widthInPixels,
+      homeMap.heightInPixels
+    );
+
+    this.homeWorld.setTileLocationCallback(0, 0, 14, 14, detectGrass, scene);
+    this.homeWorld.setTileLocationCallback(
+      7,
+      14,
+      1,
+      1,
+      detectDoor(scene, 'WorldScene', 35, 23),
+      scene
+    );
+  }
+
+  update(time, delta) {
+    this.player.update();
+  }
 }

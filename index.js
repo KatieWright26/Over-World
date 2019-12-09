a164278a2ca067eeacfd7511bf3c2aed76018ae2
Katie Wright <katie.wrght@gmail.com>
2019-12-09 15:14:55 +0100

    Updates

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ import WorldScene from './js/scenes/worldScene';
 import TreeScene from './js/scenes/treeScene';
 import HomeScene from './js/scenes/homeScene';
 
+const WIDTH = 400;
+const HEIGHT = 250;
+
 const config = {
   type: Phaser.AUTO,
-  width: 300,
-  height: 200,
-  zoom: 3,
+  width: WIDTH,
+  height: HEIGHT,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
   parent: 'game-container',
   pixelArt: true,
   scene: [WorldScene, TreeScene, HomeScene],
@@ -18,5 +24,4 @@ const config = {
     },
   },
 };
-
 const game = new Phaser.Game(config);

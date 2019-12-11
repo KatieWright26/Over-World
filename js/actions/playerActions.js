751103db71444e7f7d27bd25059cbe4a14b774db
Katie Wright <katie.wrght@gmail.com>
2019-12-11 11:02:15 +0100

    keep player idle in the original direction they were travelling in

--- js/actions/playerActions.js
+++ js/actions/playerActions.js
@@ -1,4 +1,5 @@
 function playerActions(anims) {
+  // Direction actions
   anims.create({
     key: 'player-x-walk',
     frames: anims.generateFrameNumbers('player', {
@@ -29,6 +30,9 @@ function playerActions(anims) {
     frameRate: 10,
     repeat: -1,
   });
+  // --------------------------
+  // Direction actions in grass
+  // --------------------------
   anims.create({
     key: 'player-x-walk-grass',
     frames: anims.generateFrameNumbers('player', {
@@ -59,8 +63,38 @@ function playerActions(anims) {
     frameRate: 10,
     repeat: -1,
   });
+
+  // --------------------------
+  // Idle - up - player actions
+  // --------------------------
+
   anims.create({
-    key: 'idle-player',
+    key: 'idle-up-player',
+    frames: anims.generateFrameNumbers('player', {
+      start: 8,
+      end: 8,
+      zeroPad: 1,
+    }),
+    frameRate: 3,
+    repeat: -1,
+  });
+  anims.create({
+    key: 'idle-up-player-grass',
+    frames: anims.generateFrameNumbers('player', {
+      start: 20,
+      end: 20,
+      zeroPad: 1,
+    }),
+    frameRate: 3,
+    repeat: -1,
+  });
+
+  // --------------------------
+  // Idle - down - player actions
+  // --------------------------
+
+  anims.create({
+    key: 'idle-down-player',
     frames: anims.generateFrameNumbers('player', {
       start: 24,
       end: 31,
@@ -70,10 +104,35 @@ function playerActions(anims) {
     repeat: -1,
   });
   anims.create({
-    key: 'idle-player-grass',
+    key: 'idle-down-player-grass',
     frames: anims.generateFrameNumbers('player', {
       start: 32,
       end: 39,
+      zeroPad: 1,
+    }),
+    frameRate: 3,
+    repeat: -1,
+  });
+
+  // --------------------------
+  // Idle - x- player actions
+  // --------------------------
+
+  anims.create({
+    key: 'idle-x-player',
+    frames: anims.generateFrameNumbers('player', {
+      start: 0,
+      end: 0,
+      zeroPad: 1,
+    }),
+    frameRate: 3,
+    repeat: -1,
+  });
+  anims.create({
+    key: 'idle-x-player-grass',
+    frames: anims.generateFrameNumbers('player', {
+      start: 12,
+      end: 12,
       zeroPad: 1,
     }),
     frameRate: 3,
--- js/controls/playerControls.js
+++ js/controls/playerControls.js
@@ -1,3 +1,5 @@
+let direction = 'up';
+
 const playerControls = (keys, player, speed, onGrass) => {
   if (keys.left.isDown) {
     player.body.setVelocityX(-speed);
@@ -16,36 +18,31 @@ const playerControls = (keys, player, speed, onGrass) => {
 
   // Prevent player speeding up when walking on diagonal.
   player.body.velocity.normalize().scale(speed);
+
   if (onGrass) {
     if (player.body.velocity.x !== 0) {
+      direction = 'x';
       player.anims.play('player-x-walk-grass', true);
     } else if (keys.up.isDown) {
+      direction = 'up';
       player.anims.play('player-up-walk-grass', true);
     } else if (keys.down.isDown) {
+      direction = 'down';
       player.anims.play('player-down-walk-grass', true);
     } else {
-      // if (previousFrame === 'player-down-walk-grass') {
-      //   player.anims.play("idle-player-grass", true);
-      // } else {
-      //   player.anims.stop();
-      // }
-      player.anims.stop();
-      // let lastAnim = player.anims.currentAnim.key;
-      // console.log(lastAnim)
-      // if (lastAnim === "player-down-walk-grass" || "idle-player-grass") {
-      //   player.anims.play("idle-player-grass", true);
-      // } else {
-      //   player.anims.play("player-x-walk-grass", true);
-      // }
+      player.anims.play(`idle-${direction}-player-grass`, true);
     }
   } else if (player.body.velocity.x !== 0) {
+    direction = 'x';
     player.anims.play('player-x-walk', true);
   } else if (keys.up.isDown) {
+    direction = 'up';
     player.anims.play('player-up-walk', true);
   } else if (keys.down.isDown) {
+    direction = 'down';
     player.anims.play('player-down-walk', true);
   } else {
-    player.anims.play('idle-player', true);
+    player.anims.play(`idle-${direction}-player`, true);
   }
 };
 
